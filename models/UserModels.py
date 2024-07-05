006a08b92f3cbc9ec51b8e49a1fc33aa9e76e227
Stephen Menary <stephen.menary@manchester.ac.uk>
2024-07-05 11:08:23 +0100

    Committing dummy container for user model class

---
+++ models/UserModels.py
@@ -0,0 +1,7 @@
+##
+##  UserModels.py
+##  Brief:  Gather user models implemented in separate files within this dir
+##  Initial commit:  5th July 24
+##
+
+from SimpleUserModel import SimpleUserModel
